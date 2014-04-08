2b9591f6aa4b0e7832f900b18b7e9b4d629c2f31
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2014-04-07 22:29:10 -0400

    Issue 74:	Implement better bug-reporting

--- src/AppLogFetcher.h
+++ src/AppLogFetcher.h
@@ -1,7 +1,8 @@
 #ifndef APPLOGFETCHER_H_
 #define APPLOGFETCHER_H_
 
-#include <QObject>
+#include <QMap>
+#include <QFutureWatcher>
 
 class QNetworkAccessManager;
 class QNetworkReply;
@@ -12,9 +13,23 @@ namespace bb {
     }
 }
 
+#define DEVICE_INFO_LOG QString("%1/deviceInfo.txt").arg( QDir::tempPath() )
+#define SERVICE_LOG_FILE QString("%1/logs/service.log").arg( QDir::currentPath() )
+#define UI_LOG_FILE QString("%1/logs/ui.log").arg( QDir::currentPath() )
+#define ZIP_FILE_PATH QString("%1/logs.zip").arg( QDir::tempPath() )
+
 namespace canadainc {
 
 using namespace bb::system;
+
+class LogCollector
+{
+public:
+    LogCollector() {};
+    virtual QString appName() const { return QString(); };
+    virtual QByteArray compressFiles() { return QByteArray(); };
+    ~LogCollector() {};
+};
 
 class AppLogFetcher : public QObject
 {
@@ -22,17 +37,22 @@ class AppLogFetcher : public QObject
 
     QNetworkAccessManager* m_networkManager;
     SystemProgressToast* m_progress;
-    QString m_name;
+    QFutureWatcher<QByteArray> m_future;
+    LogCollector* m_collector;
 
 private slots:
+    void onFinished();
     void onNetworkReply(QNetworkReply* reply);
+    void startCollection();
     void uploadProgress(qint64 bytesSent, qint64 bytesTotal);
-    void startCollecting();
 
 public:
-    AppLogFetcher(QString const& appName, QObject* parent=NULL);
+    AppLogFetcher(LogCollector* collector, QObject* parent=NULL);
     virtual ~AppLogFetcher();
 
+    static void dumpDeviceInfo();
+    static void enableLogging(bool enable);
+    void submitLogsLegacy();
     Q_INVOKABLE void submitLogs(bool silent=false);
 };
 
