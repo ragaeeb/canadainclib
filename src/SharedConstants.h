96a6e5eb06f738d36ed11cacbf5f5482f90015ed
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2015-09-17 21:44:46 -0400

    Add shared constants.

--- src/SharedConstants.h
+++ src/SharedConstants.h
@@ -1,6 +1,6 @@
 #ifndef SHAREDCONSTANTS_H_
 #define SHAREDCONSTANTS_H_
 
-#define NAME_FIELD(var) QString("replace( replace( replace( replace( coalesce(%1.displayName, TRIM((coalesce(%1.prefix,'') || ' ' || coalesce(%1.kunya,'') || ' ' || %1.name))),\"'\",''), '%2', ''), '%3', ''), '  ', ' ' )").arg(var).arg( QChar(8217) ).arg( QChar(8216) )
+#define NAME_FIELD(var,field) QString("replace( replace( replace( replace( coalesce(%1.displayName, TRIM((coalesce(%1.prefix,'') || ' ' || coalesce(%1.kunya,'') || ' ' || %1.name))),\"'\",''), '%2', ''), '%3', ''), '  ', ' ' ) AS %4").arg(var).arg( QChar(8217) ).arg( QChar(8216) ).arg(field)
 
 #endif /* SHAREDCONSTANTS_H_ */
