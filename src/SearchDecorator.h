f0789f7f86d197eaf6bdc6488739fe528053fcdf
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2017-02-05 00:14:46 -0500

    Expose SearchDecorator's toHtmlEscaped

--- src/SearchDecorator.h
+++ src/SearchDecorator.h
@@ -21,7 +21,7 @@ public:
     SearchDecorator(QObject* parent=NULL);
     virtual ~SearchDecorator();
 
-    static QString toHtmlEscaped(QString const& input);
+    Q_INVOKABLE static QString toHtmlEscaped(QString const& input);
     Q_INVOKABLE void decorateSimilar(QVariantList const& input, bb::cascades::ArrayDataModel* adm, bb::cascades::AbstractTextControl* atc, QString const& key);
     Q_INVOKABLE void decorateSearchResults(QVariantList const& input, bb::cascades::ArrayDataModel* adm, QVariantList const& queries, QString const& key, int index=-1);
 };
